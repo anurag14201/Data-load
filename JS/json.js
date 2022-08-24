b467ce76f4d78f54f83ff72b7057b33939fdbe33
anurag14201 <108300944+anurag14201@users.noreply.github.com>
2022-08-24 12:00:46 +0600

    data load

--- JS/json.js
+++ JS/json.js
@@ -1,3 +1,23 @@
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-    .then(response => response.json())
-    .then(json => console.log(json))
+// function displayUser() {
+//     fetch('https://jsonplaceholder.typicode.com/todos/1')
+//         .then(response => response.json())
+//         .then(data => console.log(data))
+// }
+
+function loadUser() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(data => displayUser(data));
+}
+
+
+function displayUser(data) {
+    // console.log(data);
+    const ul = document.getElementById('nothing')
+    for (const users of data) {
+        console.log(users.name);
+        const li = document.createElement('li');
+        li.innerText = users.name;
+        ul.appendChild(li);
+    }
+}
